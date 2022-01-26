487cdcb25b9b8e8d22814377ab15516485174840
3soojung <68137693+kungsae@users.noreply.github.com>
2022-01-26 17:52:51 +0900

    타임라인 건드리는중

--- YoungSan/Assets/Scripts/Timeline/TimelineController.cs
+++ YoungSan/Assets/Scripts/Timeline/TimelineController.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class TimelineController : MonoBehaviour
 {
 	PlayableDirector director;
+	[SerializeField] private Image fade;
 
 	public Entity entity;
 
@@ -20,7 +21,6 @@ public class TimelineController : MonoBehaviour
 	{
 		director.playableGraph.GetRootPlayable(0).SetSpeed(1);
 		print("Ω√¿€");
-
 	}
 	public void PauseTimeline()
 	{
@@ -31,5 +31,52 @@ public class TimelineController : MonoBehaviour
 	{
 		print(obj.name);
 		entity.GetProcessor(typeof(Processor.Animate))?.AddCommand("Play", new object[] {obj.name});
+	}
+	public void FadeInOut(bool fadeOut)
+	{
+		if (fadeOut)
+		{
+			StartCoroutine(FadeOut());
+		}
+		else
+		{
+			StartCoroutine(FadeIn());
+		}
+	}
+	private IEnumerator FadeOut()
+	{
+		float alpha = 0f;
+		while (true)
+		{
+			if (alpha < 1f)
+			{
+				alpha += Time.deltaTime * 1;
+			}
+			else
+			{
+				yield break;
+			}
+			alpha = Mathf.Clamp(alpha, 0, 1);
+			fade.color = new Color(fade.color.r, fade.color.g, fade.color.b, alpha);
+			yield return null;
+		}
+	}
+	private IEnumerator FadeIn()
+	{
+		float alpha = 1f;
+		while (true)
+		{
+			if (alpha > 0f)
+			{
+				alpha -= Time.deltaTime * 1;
+			}
+			else
+			{
+				yield break;
+			}
+			alpha = Mathf.Clamp(alpha, 0, 1);
+			fade.color = new Color(fade.color.r, fade.color.g, fade.color.b, alpha);
+			yield return null;
+		}
 	}
 }

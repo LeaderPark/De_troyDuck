fff6ee7bcdaebdcc815c9d971971c09e4b42d941
Deomyeon <deomyeon@gmail.com>
2022-02-21 16:46:52 +0900

    일단 해봐

--- YoungSan/Assets/Scripts/None/Attack.cs
+++ YoungSan/Assets/Scripts/None/Attack.cs
@@ -10,41 +10,60 @@ namespace StateMachine
         List<SkillAreaBundle> bundles = new List<SkillAreaBundle>();
         List<bool> directions = new List<bool>();
 
+        float timeStack;
+        bool start;
+
         public override State Process(StateMachine stateMachine)
         {
             GameManager gameManager = ManagerObject.Instance.GetManager(ManagerType.GameManager) as GameManager;
-            bundles.Clear();
-            directions.Clear();
             
             if (Random.Range(0, 100) < 60) return stateMachine.GetStateTable(typeof(SkillCheck));
-
-            foreach (var skillAreaBundle in stateMachine.Enemy.skillArea.skillAreaBundles)
+            
+            if (!start)
             {
-                foreach (var item in skillAreaBundle.skillAreaDatas)
+                bundles.Clear();
+                directions.Clear();
+
+                foreach (var skillAreaBundle in stateMachine.Enemy.skillArea.skillAreaBundles)
                 {
-                    bundles.Add(skillAreaBundle);
-                    if (item.inLeftSkillArea)
+                    foreach (var item in skillAreaBundle.skillAreaDatas)
                     {
-                        directions.Add(false);
-                    }
-                    if (item.inRightSkillArea)
-                    {
-                        directions.Add(true);
+                        bundles.Add(skillAreaBundle);
+                        if (item.inLeftSkillArea)
+                        {
+                            directions.Add(false);
+                        }
+                        if (item.inRightSkillArea)
+                        {
+                            directions.Add(true);
+                        }
                     }
                 }
+                int bundleIdx = Random.Range(0, bundles.Count);
+                
+                try
+                {
+                    Vector2 dirVec = new Vector2(gameManager.Player.transform.position.x, gameManager.Player.transform.position.z) - new Vector2(stateMachine.Enemy.transform.position.x, stateMachine.Enemy.transform.position.z);
+                    stateMachine.Enemy.entityEvent.CallEvent(bundles[bundleIdx].eventCategory, new object[]{dirVec.x, dirVec.y, directions[bundleIdx]});
+                }
+                catch
+                {
+                }
+                start = true;
             }
-            int bundleIdx = Random.Range(0, bundles.Count);
-            
-            try
+            if (start)
             {
-                Vector2 dirVec = new Vector2(gameManager.Player.transform.position.x, gameManager.Player.transform.position.z) - new Vector2(stateMachine.Enemy.transform.position.x, stateMachine.Enemy.transform.position.z);
-                stateMachine.Enemy.entityEvent.CallEvent(bundles[bundleIdx].eventCategory, new object[]{dirVec.x, dirVec.y, directions[bundleIdx]});
+                timeStack += Time.deltaTime;
+                
+                if (timeStack >= 0.5f)
+                {
+                    start = false;
+                    return stateMachine.GetStateTable(typeof(SkillCheck));
+                }
+                return this;
             }
-            catch
-            {
-                return stateMachine.GetStateTable(typeof(SkillCheck));
-            }
-            return stateMachine.GetStateTable(typeof(SkillCheck));
+
+            return this;
         }
     }
 }

275a9d58aef1016be946fca3ab5c27bd67f37e52
Deomyeon <deomyeon@gmail.com>
2022-03-03 14:59:08 +0900

    SkillEffect 갈아엎음

--- YoungSan/Assets/Scripts/Common/SkillEffect.cs
+++ YoungSan/Assets/Scripts/Common/SkillEffect.cs
@@ -4,6 +4,91 @@ using UnityEngine;
 
 public abstract class SkillEffect : MonoBehaviour
 {
+    public AnimationClip hitEffectClip;
+    public string hitSound;
 
-    public abstract void ShowSkillEffect(Entity attackEntity, Entity hitEntity, Vector2 direction);
+    public void ShowSkillEffect(Entity attackEntity, Entity hitEntity, Vector2 direction)
+    {
+        SoundManager soundManager = ManagerObject.Instance.GetManager(ManagerType.SoundManager) as SoundManager;
+        soundManager.SoundStart(hitSound, transform);
+        PoolManager poolManager = ManagerObject.Instance.GetManager(ManagerType.PoolManager) as PoolManager;
+        if (hitEntity != null)
+        {
+            HitEffect hitEffect = poolManager.GetObject("HitEffect").GetComponent<HitEffect>();
+            hitEffect.transform.position = hitEntity.transform.position + Vector3.up * 0.5f;
+            hitEffect.Play(hitEffectClip);
+            BloodEffect bloodEffect = poolManager.GetObject("BloodEffect").GetComponent<BloodEffect>();
+            bloodEffect.transform.position = hitEntity.transform.position;
+            bloodEffect.Play();
+        }
+        hitEntity?.GetProcessor(typeof(Processor.Animate))?.AddCommand("PlayNoLock", new object[]{"Hit"});
+
+        switch (hitEntity?.gameObject.tag)
+        {
+            case "Player": // player
+            ShowPlayerEffect(attackEntity, hitEntity, direction);
+            break;
+            case "Enemy": // enemy
+            ShowEnemyEffect(attackEntity, hitEntity, direction);
+            break;
+        }
+    }
+
+    protected abstract void ShowPlayerEffect(Entity attackEntity, Entity hitEntity, Vector2 direction);
+    protected abstract void ShowEnemyEffect(Entity attackEntity, Entity hitEntity, Vector2 direction);
+
+    protected void Stiff(Entity entity, float time)
+    {
+        entity?.GetProcessor(typeof(Processor.Move))?.AddCommand("Lock", new object[]{time});
+        entity?.GetProcessor(typeof(Processor.Animate))?.AddCommand("Lock", new object[]{time});
+    }
+
+    protected void ChangeColor(Entity entity, Color color, float time)
+    {
+        StartCoroutine(ChangeColorProcess(entity, color, time));
+    }
+
+    private IEnumerator ChangeColorProcess(Entity entity, Color color, float time)
+    {
+        entity?.GetProcessor(typeof(Processor.Sprite))?.AddCommand("SetColor", new object[] { color });
+        yield return new WaitForSeconds(time);
+        entity?.GetProcessor(typeof(Processor.Sprite))?.AddCommand("SetColor", new object[] { Color.black });
+    }
+
+    protected void KnockBack(Entity entity, Vector2 direction, float time, float power)
+    {
+        StartCoroutine(KnockBackProcess(entity, direction, time, power));
+    }
+
+    private IEnumerator KnockBackProcess(Entity entity, Vector2 direction, float time, float power)
+    {
+        if (entity != null)
+        {
+            Vector3 dir = new Vector3(direction.x, 0, direction.y).normalized;
+            float waitTime = 0;
+            while (true)
+            {
+                waitTime += Time.deltaTime;
+                if (waitTime >= time)
+                {
+                    break;
+                }
+                entity?.GetProcessor(typeof(Processor.Move))?.AddCommand("SetVelocityNoLock", new object[] { dir, power });
+                yield return null;
+            }
+            waitTime = 0;
+
+            while (true)
+            {
+                waitTime += Time.deltaTime;
+                if (waitTime >= 0.3f)
+                {
+                    break;
+                }
+                entity?.GetProcessor(typeof(Processor.Move))?.AddCommand("SetVelocityNoLock", new object[] { dir, Mathf.Lerp(power, 0, waitTime / 0.3f) });
+                yield return null;
+            }
+            entity?.GetProcessor(typeof(Processor.Move))?.AddCommand("SetVelocityNoLock", new object[] { Vector3.zero, 0 });
+        }
+    }
 }
--- YoungSan/Assets/Scripts/Data/SkillEffect/SwordMountainBandit/SwordMountainBandit_Attack2.cs
+++ YoungSan/Assets/Scripts/Data/SkillEffect/SwordMountainBandit/SwordMountainBandit_Attack2.cs
@@ -4,76 +4,17 @@ using UnityEngine;
 
 public class SwordMountainBandit_Attack2 : SkillEffect
 {
-    public AnimationClip hitEffectClip;
-
-    public override void ShowSkillEffect(Entity attackEntity, Entity hitEntity, Vector2 direction)
+    protected override void ShowPlayerEffect(Entity attackEntity, Entity hitEntity, Vector2 direction)
     {
-        SoundManager soundManager = ManagerObject.Instance.GetManager(ManagerType.SoundManager) as SoundManager;
-        soundManager.SoundStart("HitSound2", transform);
-        switch (hitEntity?.gameObject.tag)
-        {
-            case "Player": // player
-            hitEntity?.GetProcessor(typeof(Processor.Move))?.AddCommand("Lock", new object[]{0.2f});
-            hitEntity?.GetProcessor(typeof(Processor.Animate))?.AddCommand("PlayNoLock", new object[]{"Hit"});
-            hitEntity?.GetProcessor(typeof(Processor.Animate))?.AddCommand("Lock", new object[]{0.2f});
-            StartCoroutine(DamageColor(hitEntity));
-            StartCoroutine(KnockBack(hitEntity, direction, 0.1f, 8));
-            break;
-            case "Enemy": // enemy
-            hitEntity?.GetProcessor(typeof(Processor.Animate))?.AddCommand("PlayNoLock", new object[]{"Hit"});
-            hitEntity?.GetProcessor(typeof(Processor.Animate))?.AddCommand("Lock", new object[]{0.4f});
-            hitEntity?.GetProcessor(typeof(Processor.Move))?.AddCommand("Lock", new object[]{0.4f});
-            StartCoroutine(DamageColor(hitEntity));
-            StartCoroutine(KnockBack(hitEntity, direction, 0.2f, 8));
-            break;
-        }
-        PoolManager poolManager = ManagerObject.Instance.GetManager(ManagerType.PoolManager) as PoolManager;
-        if (hitEntity != null)
-        {
-            HitEffect hitEffect = poolManager.GetObject("HitEffect").GetComponent<HitEffect>();
-            hitEffect.transform.position = hitEntity.transform.position + Vector3.up * 0.5f;
-            hitEffect.Play(hitEffectClip);
-            BloodEffect bloodEffect = poolManager.GetObject("BloodEffect").GetComponent<BloodEffect>();
-            bloodEffect.transform.position = hitEntity.transform.position;
-            bloodEffect.Play();
-        }
+        Stiff(hitEntity, 0.2f);
+        ChangeColor(hitEntity, Color.white, 0.1f);
+        KnockBack(hitEntity, direction, 0.1f, 8);
     }
 
-    IEnumerator KnockBack(Entity hitEntity, Vector2 direction, float time, float power)
+    protected override void ShowEnemyEffect(Entity attackEntity, Entity hitEntity, Vector2 direction)
     {
-        if (hitEntity != null)
-        {
-            Vector3 dir = new Vector3(direction.x, 0, direction.y).normalized;
-            float waitTime = 0;
-            while (true)
-            {
-                waitTime += Time.deltaTime;
-                if (waitTime >= time)
-                {
-                    break;
-                }
-                hitEntity?.GetProcessor(typeof(Processor.Move))?.AddCommand("SetVelocityNoLock", new object[] { dir, power });
-                yield return null;
-            }
-            waitTime = 0;
-
-            while (true)
-            {
-                waitTime += Time.deltaTime;
-                if (waitTime >= 0.3f)
-                {
-                    break;
-                }
-                hitEntity?.GetProcessor(typeof(Processor.Move))?.AddCommand("SetVelocityNoLock", new object[] { dir, Mathf.Lerp(power, 0, waitTime / 0.3f) });
-                yield return null;
-            }
-            hitEntity?.GetProcessor(typeof(Processor.Move))?.AddCommand("SetVelocityNoLock", new object[] { Vector3.zero, 0 });
-        }
-    }
-    IEnumerator DamageColor(Entity hitEntity)
-    {
-        hitEntity?.GetProcessor(typeof(Processor.Sprite))?.AddCommand("SetColor", new object[] { Color.white });
-        yield return new WaitForSeconds(0.1f);
-        hitEntity?.GetProcessor(typeof(Processor.Sprite))?.AddCommand("SetColor", new object[] { Color.black });
+        Stiff(hitEntity, 0.4f);
+        ChangeColor(hitEntity, Color.white, 0.1f);
+        KnockBack(hitEntity, direction, 0.2f, 8);
     }
 }

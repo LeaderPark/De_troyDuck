d33c7899354a583a813d11a8831c3717945ea472
3soojung <68137693+kungsae@users.noreply.github.com>
2022-03-03 12:30:48 +0900

    왠지 모르지만 버그 고침

--- YoungSan/Assets/Scripts/Manager/TimelineManager.cs
+++ YoungSan/Assets/Scripts/Manager/TimelineManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class TimelineManager : Manager
 {

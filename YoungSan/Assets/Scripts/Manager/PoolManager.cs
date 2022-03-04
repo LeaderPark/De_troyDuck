da2fda6f4ba417d937fa98260ed814d39e535919
Deomyeon <deomyeon@gmail.com>
2022-03-05 01:42:52 +0900

    문제해결

--- YoungSan/Assets/Scripts/Manager/PoolManager.cs
+++ YoungSan/Assets/Scripts/Manager/PoolManager.cs
@@ -6,11 +6,13 @@ public class PoolManager : Manager
 {
     Dictionary<string, List<GameObject>> PoolObjects;
     Hashtable PoolObjectTable {get; set;}
+    private GameObject canvas;
 
     void Awake()
     {
         PoolObjects = new Dictionary<string, List<GameObject>>();
         PoolObjectTable = new Hashtable();
+        canvas = transform.Find("Canvas").gameObject;
         
         LoadPoolObjects();
     }
@@ -40,6 +42,27 @@ public class PoolManager : Manager
             if (PoolObjectTable.ContainsKey(name))
             {
                 PoolObjects[name].Add(GameObject.Instantiate((GameObject)PoolObjectTable[name], transform));
+                return PoolObjects[name][PoolObjects[name].Count - 1];
+            }
+        }
+        return null;
+    }
+
+    public GameObject GetUIObject(string name)
+    {
+        if (PoolObjects.ContainsKey(name))
+        {
+            foreach (GameObject item in PoolObjects[name])
+            {
+                if (!item.activeSelf)
+                {
+                    item.SetActive(true);
+                    return item;
+                }
+            }
+            if (PoolObjectTable.ContainsKey(name))
+            {
+                PoolObjects[name].Add(GameObject.Instantiate((GameObject)PoolObjectTable[name], canvas.transform));
                 return PoolObjects[name][PoolObjects[name].Count - 1];
             }
         }

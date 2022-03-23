f5822bd25b5e57a1b35495074da071cf2cf57bb8
Deomyeon <deomyeon@gmail.com>
2022-03-23 21:57:00 +0900

    응애

--- YoungSan/Assets/Scripts/Quest/Quest.cs
+++ YoungSan/Assets/Scripts/Quest/Quest.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quest
+[CreateAssetMenu(fileName = "Quest", menuName = "ScriptableObjects/Quest", order = 1)]
+public class Quest : ScriptableObject
 {
+    [TextArea()]
+    public string title;
+    [TextArea()]
+    public string context;
+    public Quest prevQuest;
+    public Quest nextQuest;
+
     
 }

690b1c09afd1b5c619623128736bd88cf0883e43
LeaderPark <dkdleldjqkr976@gmail.com>
2022-05-03 12:44:00 +0900

    에잉 쌉텐의재련
    에잉 쌉텐의재련

--- YoungSan/Assets/Scripts/Manager/QuestManager.cs
+++ YoungSan/Assets/Scripts/Manager/QuestManager.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class QuestManager : Manager
 {
     public Hashtable proceedingQuests = new Hashtable();
-    private Hashtable completedQuests;
-    
-    public System.Action OnChanged; 
+    public Hashtable completedQuests = new Hashtable();
 
     public void AddQuest(Quest quest)
     {
         proceedingQuests.Add(quest.questId, quest);
-        OnChanged?.Invoke();
     }
 
     public void RemoveQuest(Quest quest)
     {
         proceedingQuests.Remove(quest.questId);
-        OnChanged?.Invoke();
     }
+
+    public void ClearQuest(Quest quest)
+    {
+        proceedingQuests.Remove(quest.questId);
+        completedQuests.Add(quest.questId, quest);
+    }
+
+    public void CheckAvailableQuest()
+    {
+
+    }
+
 }

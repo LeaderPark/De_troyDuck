3bbab7ff81ad429690ef07c2e76da9ea2b75d692
Deomyeon <deomyeon@gmail.com>
2022-05-19 20:10:56 +0900

    와 재화

--- YoungSan/Assets/Scripts/Product.cs
+++ YoungSan/Assets/Scripts/Product.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Product : MonoBehaviour
+[CreateAssetMenu(fileName = "Product", menuName = "ScriptableObjects/Product", order = 1)]
+public class Product : ScriptableObject
 {
+    [Space(40)]
+    public string productName;
+    [Space(10)]
+    public int productCount;
+    [Space(10)]
+    public Sprite productSprite;
     
 }

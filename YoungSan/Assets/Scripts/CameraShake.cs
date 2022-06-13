fa0cd2d660d879aace6ea7f4beb2f46f8c8ca98f
3soojung <68137693+kungsae@users.noreply.github.com>
2022-06-13 18:18:11 +0900

    대사들 수정
    대사들 수정

--- YoungSan/Assets/Scripts/CameraShake.cs
+++ YoungSan/Assets/Scripts/CameraShake.cs
@@ -23,6 +23,10 @@ public class CameraShake : MonoBehaviour
     {
         camShakePower = power;
     }
+    public void LerpShake(int count, float delay)
+    {
+        StartCoroutine(ShakeRepeaLerpt(Camera.main.GetComponent<Cinemachine.CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>(), count, delay));
+    }
     public void Shake1(int count,float delay)
     {
         StartCoroutine(ShakeRepeat(Camera.main.GetComponent<Cinemachine.CinemachineBrain>().ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>(), count, delay));
@@ -40,6 +44,18 @@ public class CameraShake : MonoBehaviour
             Vector3 pos = virtualCam.transform.position;
             Quaternion rot = virtualCam.transform.rotation;
             virtualCam.ForceCameraPosition(pos + new Vector3(Random.Range(-0.5f, 0.5f)* camShakePower/*- 0.5f */, Random.Range(-0.5f, 0.5f)*camShakePower/* - 0.5f*/, 0), rot);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+    IEnumerator ShakeRepeaLerpt(CinemachineVirtualCamera virtualCam, int count, float delay)
+    {
+        for (int i = count; i >= 0; i--)
+        {
+            Vector3 pos = virtualCam.transform.position;
+            Quaternion rot = virtualCam.transform.rotation;
+            float _camShakePower = Mathf.Lerp(0, camShakePower, (float)i / (float)count);
+            Debug.Log(_camShakePower);
+            virtualCam.ForceCameraPosition(pos + new Vector3(Random.Range(-0.5f, 0.5f) * _camShakePower/*- 0.5f */, Random.Range(-0.5f, 0.5f) * _camShakePower/* - 0.5f*/, 0), rot);
             yield return new WaitForSeconds(delay);
         }
     }

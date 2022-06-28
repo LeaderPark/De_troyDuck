adf33f6b4921b8a68c8f5830e5b9906d1fc795df
Deomyeon <deomyeon@gmail.com>
2022-06-28 21:23:28 +0900

    이얏 텍스트 컬러다!!

--- YoungSan/Assets/Scripts/Timeline/Test/TestBehaivor.cs
+++ YoungSan/Assets/Scripts/Timeline/Test/TestBehaivor.cs
@@ -9,219 +9,250 @@ using UnityEngine.UI;
 
 public class TestBehaivor : PlayableBehaviour
 {
-	public GameObject talker;
-	public string txt;
-	public GameObject talkObj;
-	public AnimationCurve delayCurve;
-	public AnimationCurve fontSizeCurve;
+    public GameObject talker;
+    public string txt;
+    public GameObject talkObj;
+    public AnimationCurve delayCurve;
+    public AnimationCurve fontSizeCurve;
+    public TextColor[] textColors;
 
-	private Vector3[] vertice;
+    private Vector3[] vertice;
 
-	private TextMeshProUGUI talkBox;
-	private TextMeshProUGUI fakeTalkbox;
-	//private GameObject endImage;
-	private PoolManager poolManager;
-	private UIManager uiManager;
+    private TextMeshProUGUI talkBox;
+    private TextMeshProUGUI fakeTalkbox;
+    //private GameObject endImage;
+    private PoolManager poolManager;
+    private UIManager uiManager;
 
-	private float time = 0;
-	private int idx = 0;
-	private EntityData entityData;
-	private TimelineController timelineCon;
+    private float time = 0;
+    private int idx = 0;
+    private EntityData entityData;
+    private TimelineController timelineCon;
 
-	private Coroutine textOut;
+    private Coroutine textOut;
 
 
-	//Å¸ÀÓ¶óÀÎ ½ÃÀÛÇÏ¸é ½ÇÇà
-	public override void OnGraphStart(Playable playable)
-	{
-		if (Application.isPlaying)
-		{
-			if (ManagerObject.Instance != null)
-			{
-				poolManager = ManagerObject.Instance.GetManager(ManagerType.PoolManager) as PoolManager;
-				uiManager = ManagerObject.Instance.GetManager(ManagerType.UIManager) as UIManager;
-			}
-		}
-		//Debug.Log("AAA");
-	}
-	//ÇÃ·¹ÀÌ¾îºí Æ®·¢ÀÌ ½ÇÇàµÉ¶§ ÇÑ¹ø ½ÇÇà
-	public override void OnBehaviourPlay(Playable playable, FrameData info)
-	{
-		if (Application.isPlaying)
-		{
-			talkObj = poolManager.GetUIObject("TalkBoxTMP");
-		}
-		else
-		{
-			List<GameObject> pool = new List<GameObject>();
-			GameObject canvas = GameObject.Find("CutSceneCanvas");
+    //Å¸ï¿½Ó¶ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ï¸ï¿½ ï¿½ï¿½ï¿½ï¿½
+    public override void OnGraphStart(Playable playable)
+    {
+        if (Application.isPlaying)
+        {
+            if (ManagerObject.Instance != null)
+            {
+                poolManager = ManagerObject.Instance.GetManager(ManagerType.PoolManager) as PoolManager;
+                uiManager = ManagerObject.Instance.GetManager(ManagerType.UIManager) as UIManager;
+            }
+        }
+        //Debug.Log("AAA");
+    }
+    //ï¿½Ã·ï¿½ï¿½Ì¾ï¿½ï¿½ Æ®ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½É¶ï¿½ ï¿½Ñ¹ï¿½ ï¿½ï¿½ï¿½ï¿½
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        if (Application.isPlaying)
+        {
+            talkObj = poolManager.GetUIObject("TalkBoxTMP");
+        }
+        else
+        {
+            List<GameObject> pool = new List<GameObject>();
+            GameObject canvas = GameObject.Find("CutSceneCanvas");
 
-			time = 0;
-			idx = 0;
-			for (int i = 0; i < canvas.transform.childCount-1; i++)
-			{
-				pool.Add(canvas.transform.GetChild(i).gameObject);
-			}
-			foreach (var item in pool)
-			{
-				if (!item.activeSelf)
-				{
-					item.SetActive(true);
-					talkObj = item;
-					break;
-				}
-			}
-			if (talkObj == null)
-			{
-				talkObj = GameObject.Instantiate(Resources.Load<GameObject>("PoolObject/TalkBoxTMP"),canvas.transform);
-			}
-			
-		}
-		talkBox = talkObj.transform.Find("text").GetComponent<TextMeshProUGUI>();
-		fakeTalkbox = talkObj.transform.Find("fakeText").GetComponent<TextMeshProUGUI>();
-		//endImage = talkObj.transform.Find("EndImage").gameObject;
-		timelineCon = GameObject.Find("CutScenePrefab").GetComponent<TimelineController>();
-		entityData = talker.GetComponent<Entity>().entityData;
-		//talkBox.text = txt;
-		talkBox.text = "";
-		for (int i = 0; i < txt.Length; i++)
-		{
-			StringBuilder stringBuilder = new StringBuilder();
-			stringBuilder.Append("<size=" + fontSizeCurve[i].value + ">" + txt[i] + "</size>");
-			//fakeTalkbox.text += stringBuilder;
-			talkBox.text += stringBuilder;
-		}
-		fakeTalkbox.text = "";
-		textOut = uiManager.StartCoroutine(uiManager.TextAnimationPlay(talkBox, delayCurve));
+            time = 0;
+            idx = 0;
+            for (int i = 0; i < canvas.transform.childCount - 1; i++)
+            {
+                pool.Add(canvas.transform.GetChild(i).gameObject);
+            }
+            foreach (var item in pool)
+            {
+                if (!item.activeSelf)
+                {
+                    item.SetActive(true);
+                    talkObj = item;
+                    break;
+                }
+            }
+            if (talkObj == null)
+            {
+                talkObj = GameObject.Instantiate(Resources.Load<GameObject>("PoolObject/TalkBoxTMP"), canvas.transform);
+            }
 
-		talkObj.SetActive(true);
-		SetBoxSize();
+        }
+        talkBox = talkObj.transform.Find("text").GetComponent<TextMeshProUGUI>();
+        fakeTalkbox = talkObj.transform.Find("fakeText").GetComponent<TextMeshProUGUI>();
+        //endImage = talkObj.transform.Find("EndImage").gameObject;
+        timelineCon = GameObject.Find("CutScenePrefab").GetComponent<TimelineController>();
+        entityData = talker.GetComponent<Entity>().entityData;
+        //talkBox.text = txt;
+        talkBox.text = "";
+        Color[] colors = new Color[txt.Length];
+        for (int i = 0; i < textColors.Length; i++)
+        {
+            for (int j = 0; j < txt.Length; j++)
+            {
+                if (j >= textColors[i].startIndex && j < textColors[i].endIndex)
+                {
+                    colors[j] = textColors[i].color;
+                }
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < txt.Length; i++)
+        {
+            stringBuilder.Append("<color=#");
+            stringBuilder.AppendFormat("{0:x2}", (int)(colors[i].r * 255));
+            stringBuilder.AppendFormat("{0:x2}", (int)(colors[i].g * 255));
+            stringBuilder.AppendFormat("{0:x2}", (int)(colors[i].b * 255));
+            stringBuilder.Append("ff>");
+            Debug.Log(stringBuilder.ToString());
+            stringBuilder.Append("<size=");
+            stringBuilder.Append(fontSizeCurve[i].value);
+            stringBuilder.Append(">");
+            stringBuilder.Append(txt[i]);
+            stringBuilder.Append("</size>");
+            stringBuilder.Append("</color>");
+        }
+        talkBox.text = stringBuilder.ToString();
+        Debug.Log(talkBox.text);
+        fakeTalkbox.text = "";
+        textOut = uiManager.StartCoroutine(uiManager.TextAnimationPlay(talkBox, delayCurve));
 
-	}
-	//Å¸ÀÓ¶óÀÎ ÆÛÁî °É¸®¸é ½ÇÇà(³¡³¯¶§µµ ½ÇÇà)
-	public override void OnBehaviourPause(Playable playable, FrameData info)
-	{
-		if (Application.isPlaying)
-		{
-			if (talkObj != null)
-			{
-				talkObj.SetActive(false);
-				uiManager.StopCoroutine(textOut);
-				//if ((int)playable.GetTime() >= (int)playable.GetDuration())
-				//{
-				//	//endImage.SetActive(false);
-				//	talkObj.SetActive(false);
-				//}
-			}
-		}
-		else
-		{
-			if(talkObj!=null)
-			talkObj.SetActive(false);
-		}
-	}
-	//ÇÃ·¹ÀÌ¾îºí Æ®·¢ ½ÇÇàÁß¿¡ °è¼Ó ½ÇÇà
-	public override void ProcessFrame(Playable playable, FrameData info, object playerData)
-	{
-		if (talker == null || delayCurve.length == 0)
-			return;
-		vertice = talkBox.mesh.vertices;
-		if (Application.isPlaying)
-		{
-			talkObj.transform.position = Camera.main.WorldToScreenPoint(talker.transform.position+new Vector3(0,entityData.uiPos,0));
-			time += Time.deltaTime* (float)playable.GetGraph().GetRootPlayable(0).GetSpeed();
-			//endImage.SetActive(timelineCon.timelinePause);
-			//for (bool b = true; b;)
-			//{
-			//	b = false;
-			//	if (time >= delayCurve.Evaluate(idx * 0.1f))
-			//	{
-			//		b = true;
-			//		time -= delayCurve.Evaluate(idx * 0.1f);
-			//		if (idx < txt.Length)
-			//		{
-			//			//StringBuilder stringBuilder = new StringBuilder();
-			//			//stringBuilder.Append("<size="+fontSizeCurve[idx].value+">"+txt[idx]+"</size>");
-			//			//talkBox.text += stringBuilder;
-			//			uiManager.StartCoroutine(uiManager.TextAnimation(idx, talkBox, vertice));
-			//			idx = Mathf.Clamp(idx + 1, 0, txt.Length - 1);
-			//		}
-			//	}
-			//}
-			//if (time >= delayCurve.Evaluate(idx * 0.1f))
-			//{
-			//	time = 0;
-			//	uiManager.StartCoroutine(uiManager.TextAnimation(idx, talkBox, vertice));
-			//	idx = Mathf.Clamp(idx + 1, 0, txt.Length - 1);
-			//}
-		}
-		else
-		{
-			try
-			{
-			talkObj.transform.position = Camera.main.WorldToScreenPoint(talker.transform.position + new Vector3(0, entityData.uiPos, 0));
-			time += Time.deltaTime;
-			for (bool b = true; b;)
-			{
-				b = false;
-				if (time >= delayCurve.Evaluate(idx * 0.1f))
-				{
-					b = true;
-					time -= delayCurve.Evaluate(idx * 0.1f);
-					if (idx < txt.Length)
-					{
-						talkBox.text += txt[idx];
-						idx = Mathf.Clamp(idx + 1, 0, txt.Length);
-					}
-				}
-			}
-			}
-			catch
-			{
-				
-			}
-		}
-	}
-	public void SetBoxSize()
-	{
-		talkBox.rectTransform.anchoredPosition = new Vector2(0, 0);
-		float x = talkBox.preferredWidth;
-		float y = talkBox.preferredHeight;
-		//float x = fakeTalkbox.preferredWidth;
-		//float y = fakeTalkbox.preferredHeight;
-		RectTransform talkBoxRect = talkObj.GetComponent<RectTransform>();
-		talkBoxRect.sizeDelta = new Vector2(x, y) + new Vector2(50,50);
-		talkBox.rectTransform.anchoredPosition += new Vector2(50/2 ,0);
-	}
-	//public IEnumerator TextAnimation(int idx, TextMeshProUGUI talkBox)
-	//{
-	//	Mesh mesh = talkBox.mesh;
-	//	Vector3[] vertice = talkBox.mesh.vertices;
-	//	Vector3[] origineVertice = talkBox.mesh.vertices;
-	//	Color32[] vertexColors = talkBox.textInfo.meshInfo[0].colors32;
-	//	float time = 0;
-	//	while (true)
-	//	{
-	//		time += Time.deltaTime;
-	//		vertice[idx * 4 + 0] = Vector3.Lerp(origineVertice[idx * 4 + 0], origineVertice[idx * 4 + 0] + new Vector3(0, 10, 0), time / 0.1f);
-	//		vertice[idx * 4 + 1] = Vector3.Lerp(origineVertice[idx * 4 + 1], origineVertice[idx * 4 + 1] + new Vector3(0, 10, 0), time / 0.1f);
-	//		vertice[idx * 4 + 2] = Vector3.Lerp(origineVertice[idx * 4 + 2], origineVertice[idx * 4 + 2] + new Vector3(0, 10, 0), time / 0.1f);
-	//		vertice[idx * 4 + 3] = Vector3.Lerp(origineVertice[idx * 4 + 3], origineVertice[idx * 4 + 3] + new Vector3(0, 10, 0), time / 0.1f);
+        talkObj.SetActive(true);
+        SetBoxSize();
 
-	//		vertexColors[idx * 4 + 0].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
-	//		vertexColors[idx * 4 + 1].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
-	//		vertexColors[idx * 4 + 2].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
-	//		vertexColors[idx * 4 + 3].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
+    }
+    //Å¸ï¿½Ó¶ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½É¸ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½(ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½)
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if (Application.isPlaying)
+        {
+            if (talkObj != null)
+            {
+                talkObj.SetActive(false);
+                uiManager.StopCoroutine(textOut);
+                //if ((int)playable.GetTime() >= (int)playable.GetDuration())
+                //{
+                //	//endImage.SetActive(false);
+                //	talkObj.SetActive(false);
+                //}
+            }
+        }
+        else
+        {
+            if (talkObj != null)
+                talkObj.SetActive(false);
+        }
+    }
+    //ï¿½Ã·ï¿½ï¿½Ì¾ï¿½ï¿½ Æ®ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ß¿ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        if (talker == null || delayCurve.length == 0)
+            return;
+        vertice = talkBox.mesh.vertices;
+        if (Application.isPlaying)
+        {
+            talkObj.transform.position = Camera.main.WorldToScreenPoint(talker.transform.position + new Vector3(0, entityData.uiPos, 0));
+            time += Time.deltaTime * (float)playable.GetGraph().GetRootPlayable(0).GetSpeed();
+            //endImage.SetActive(timelineCon.timelinePause);
+            //for (bool b = true; b;)
+            //{
+            //	b = false;
+            //	if (time >= delayCurve.Evaluate(idx * 0.1f))
+            //	{
+            //		b = true;
+            //		time -= delayCurve.Evaluate(idx * 0.1f);
+            //		if (idx < txt.Length)
+            //		{
+            //			//StringBuilder stringBuilder = new StringBuilder();
+            //			//stringBuilder.Append("<size="+fontSizeCurve[idx].value+">"+txt[idx]+"</size>");
+            //			//talkBox.text += stringBuilder;
+            //			uiManager.StartCoroutine(uiManager.TextAnimation(idx, talkBox, vertice));
+            //			idx = Mathf.Clamp(idx + 1, 0, txt.Length - 1);
+            //		}
+            //	}
+            //}
+            //if (time >= delayCurve.Evaluate(idx * 0.1f))
+            //{
+            //	time = 0;
+            //	uiManager.StartCoroutine(uiManager.TextAnimation(idx, talkBox, vertice));
+            //	idx = Mathf.Clamp(idx + 1, 0, txt.Length - 1);
+            //}
+        }
+        else
+        {
+            try
+            {
+                talkObj.transform.position = Camera.main.WorldToScreenPoint(talker.transform.position + new Vector3(0, entityData.uiPos, 0));
+                time += Time.deltaTime;
+                for (bool b = true; b;)
+                {
+                    b = false;
+                    if (time >= delayCurve.Evaluate(idx * 0.1f))
+                    {
+                        b = true;
+                        time -= delayCurve.Evaluate(idx * 0.1f);
+                        if (idx < txt.Length)
+                        {
+                            talkBox.text += txt[idx];
+                            idx = Mathf.Clamp(idx + 1, 0, txt.Length);
+                        }
+                    }
+                }
+            }
+            catch
+            {
 
-	//		talkBox.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
-	//		mesh.vertices = vertice;
-	//		talkBox.canvasRenderer.SetMesh(mesh);
+            }
+        }
+    }
+    public void SetBoxSize()
+    {
+        talkBox.rectTransform.anchoredPosition = new Vector2(0, 0);
+        float x = talkBox.preferredWidth;
+        float y = talkBox.preferredHeight;
+        //float x = fakeTalkbox.preferredWidth;
+        //float y = fakeTalkbox.preferredHeight;
+        RectTransform talkBoxRect = talkObj.GetComponent<RectTransform>();
+        talkBoxRect.sizeDelta = new Vector2(x, y) + new Vector2(50, 50);
+        talkBox.rectTransform.anchoredPosition += new Vector2(50 / 2, 0);
+    }
+    //public IEnumerator TextAnimation(int idx, TextMeshProUGUI talkBox)
+    //{
+    //	Mesh mesh = talkBox.mesh;
+    //	Vector3[] vertice = talkBox.mesh.vertices;
+    //	Vector3[] origineVertice = talkBox.mesh.vertices;
+    //	Color32[] vertexColors = talkBox.textInfo.meshInfo[0].colors32;
+    //	float time = 0;
+    //	while (true)
+    //	{
+    //		time += Time.deltaTime;
+    //		vertice[idx * 4 + 0] = Vector3.Lerp(origineVertice[idx * 4 + 0], origineVertice[idx * 4 + 0] + new Vector3(0, 10, 0), time / 0.1f);
+    //		vertice[idx * 4 + 1] = Vector3.Lerp(origineVertice[idx * 4 + 1], origineVertice[idx * 4 + 1] + new Vector3(0, 10, 0), time / 0.1f);
+    //		vertice[idx * 4 + 2] = Vector3.Lerp(origineVertice[idx * 4 + 2], origineVertice[idx * 4 + 2] + new Vector3(0, 10, 0), time / 0.1f);
+    //		vertice[idx * 4 + 3] = Vector3.Lerp(origineVertice[idx * 4 + 3], origineVertice[idx * 4 + 3] + new Vector3(0, 10, 0), time / 0.1f);
 
-	//		if (time > 0.1f)
-	//		{
-	//			yield break;
-	//		}
-	//	}
-	//	yield return null;
-	//}
+    //		vertexColors[idx * 4 + 0].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
+    //		vertexColors[idx * 4 + 1].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
+    //		vertexColors[idx * 4 + 2].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
+    //		vertexColors[idx * 4 + 3].a = (byte)Mathf.Lerp(255, 0, time / 0.1f);
+
+    //		talkBox.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
+    //		mesh.vertices = vertice;
+    //		talkBox.canvasRenderer.SetMesh(mesh);
+
+    //		if (time > 0.1f)
+    //		{
+    //			yield break;
+    //		}
+    //	}
+    //	yield return null;
+    //}
+}
+
+[System.Serializable]
+public class TextColor
+{
+    public int startIndex;
+    public int endIndex;
+    public Color color;
 }
--- YoungSan/Assets/Scripts/Timeline/Test/TextAsset.cs
+++ YoungSan/Assets/Scripts/Timeline/Test/TextAsset.cs
@@ -7,105 +7,107 @@ using UnityEngine.Timeline;
 
 public class TextAsset : PlayableAsset
 {
-	public ExposedReference<GameObject> talker;
-	public string dialogueMessage;
+    public ExposedReference<GameObject> talker;
+    public string dialogueMessage;
 
-	public AnimationCurve delayCurve;
-	public AnimationCurve fontSizeCurve;
-	public float activeTime;
+    public AnimationCurve delayCurve;
+    public AnimationCurve fontSizeCurve;
+    public TextColor[] textColors;
+    public float activeTime;
 
-	public ScriptPlayable<TestBehaivor> playable;
+    public ScriptPlayable<TestBehaivor> playable;
 
-	public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
-	{
-		playable = ScriptPlayable<TestBehaivor>.Create(graph);
-		var behaviour = playable.GetBehaviour();
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        playable = ScriptPlayable<TestBehaivor>.Create(graph);
+        var behaviour = playable.GetBehaviour();
 
-		if (Application.isPlaying)
-		{
-			//if (ManagerObject.Instance != null)
-			//{
-			//	PoolManager poolManager = ManagerObject.Instance.GetManager(ManagerType.PoolManager) as PoolManager;
-			//	behaviour.talkObj = poolManager.GetUIObject("TalkBox");
-				
-			//}
-		}
-		else
-		{
-			//GameObject talkObj = Instantiate(Resources.Load<GameObject>("PoolObject/TalkBox"));
-			//behaviour.talkObj = talkObj;
-			//behaviour.talkObj.transform.parent = GameObject.Find("TestCanvas").transform;
-		}
-		//behaviour.talkObj.SetActive(false);
-		behaviour.delayCurve = delayCurve;
-		behaviour.fontSizeCurve = fontSizeCurve;
-		behaviour.talker = talker.Resolve(graph.GetResolver());
-		behaviour.txt = dialogueMessage;
+        if (Application.isPlaying)
+        {
+            //if (ManagerObject.Instance != null)
+            //{
+            //	PoolManager poolManager = ManagerObject.Instance.GetManager(ManagerType.PoolManager) as PoolManager;
+            //	behaviour.talkObj = poolManager.GetUIObject("TalkBox");
 
-		return playable;
-	}
+            //}
+        }
+        else
+        {
+            //GameObject talkObj = Instantiate(Resources.Load<GameObject>("PoolObject/TalkBox"));
+            //behaviour.talkObj = talkObj;
+            //behaviour.talkObj.transform.parent = GameObject.Find("TestCanvas").transform;
+        }
+        //behaviour.talkObj.SetActive(false);
+        behaviour.delayCurve = delayCurve;
+        behaviour.fontSizeCurve = fontSizeCurve;
+        behaviour.textColors = textColors;
+        behaviour.talker = talker.Resolve(graph.GetResolver());
+        behaviour.txt = dialogueMessage;
+
+        return playable;
+    }
 }
 #if UNITY_EDITOR
 [CustomEditor(typeof(TextAsset))]
 public class TestAssetEditor : Editor
 {
-	public override void OnInspectorGUI()
-	{
-		base.OnInspectorGUI();
-		if (GUILayout.Button("Set Clip Size"))
-		{
-			TextAsset ta = (TextAsset)serializedObject.targetObject;
-			if (ta.playable.IsNull()) return;
-			for (int i = 0; i < ta.playable.GetGraph().GetOutputCount(); i++)
-			{
-				TrackAsset asset = ta.playable.GetGraph().GetOutput(i).GetReferenceObject() as TrackAsset;
-				if (asset == null) continue;
-				foreach (var clip in asset.GetClips())
-				{
-					if (clip.asset == ta)
-					{
-						double dur = 0;
-						for (int j = 0; j < ta.dialogueMessage.Length; j++)
-						{
-							dur += ta.delayCurve.Evaluate(j * 0.1f);
-						}
-						clip.duration = dur+ ta.activeTime;
-					}
-				}
-			}
-		}
-		if (GUILayout.Button("Set Default Curve"))
-		{
-			TextAsset ta = (TextAsset)serializedObject.targetObject;
-			Keyframe[] keyframes = new Keyframe[ta.dialogueMessage.Length];
-			for (int i = 0; i < ta.dialogueMessage.Length; i++)
-			{
-				keyframes[i].time = 0.1f * i;
-				keyframes[i].value = 0.05f;
-			}
-			ta.delayCurve.keys = keyframes;
-			for (int i = 0; i < ta.dialogueMessage.Length; i++)
-			{
-				AnimationUtility.SetKeyLeftTangentMode(ta.delayCurve, i, AnimationUtility.TangentMode.Linear);
-				AnimationUtility.SetKeyRightTangentMode(ta.delayCurve, i, AnimationUtility.TangentMode.Linear);
-			}
-		}
-		if (GUILayout.Button("Set Default FontSize"))
-		{
-			TextAsset ta = (TextAsset)serializedObject.targetObject;
-			Keyframe[] keyframes = new Keyframe[ta.dialogueMessage.Length];
-			for (int i = 0; i < ta.dialogueMessage.Length; i++)
-			{
-				keyframes[i].time = 0.1f * i;
-				keyframes[i].value = 30f;
-			}
-			ta.fontSizeCurve.keys = keyframes;
-			for (int i = 0; i < ta.dialogueMessage.Length; i++)
-			{
-				AnimationUtility.SetKeyLeftTangentMode(ta.fontSizeCurve, i, AnimationUtility.TangentMode.Linear);
-				AnimationUtility.SetKeyRightTangentMode(ta.fontSizeCurve, i, AnimationUtility.TangentMode.Linear);
-			}
-		}
-	}
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Set Clip Size"))
+        {
+            TextAsset ta = (TextAsset)serializedObject.targetObject;
+            if (ta.playable.IsNull()) return;
+            for (int i = 0; i < ta.playable.GetGraph().GetOutputCount(); i++)
+            {
+                TrackAsset asset = ta.playable.GetGraph().GetOutput(i).GetReferenceObject() as TrackAsset;
+                if (asset == null) continue;
+                foreach (var clip in asset.GetClips())
+                {
+                    if (clip.asset == ta)
+                    {
+                        double dur = 0;
+                        for (int j = 0; j < ta.dialogueMessage.Length; j++)
+                        {
+                            dur += ta.delayCurve.Evaluate(j * 0.1f);
+                        }
+                        clip.duration = dur + ta.activeTime;
+                    }
+                }
+            }
+        }
+        if (GUILayout.Button("Set Default Curve"))
+        {
+            TextAsset ta = (TextAsset)serializedObject.targetObject;
+            Keyframe[] keyframes = new Keyframe[ta.dialogueMessage.Length];
+            for (int i = 0; i < ta.dialogueMessage.Length; i++)
+            {
+                keyframes[i].time = 0.1f * i;
+                keyframes[i].value = 0.05f;
+            }
+            ta.delayCurve.keys = keyframes;
+            for (int i = 0; i < ta.dialogueMessage.Length; i++)
+            {
+                AnimationUtility.SetKeyLeftTangentMode(ta.delayCurve, i, AnimationUtility.TangentMode.Linear);
+                AnimationUtility.SetKeyRightTangentMode(ta.delayCurve, i, AnimationUtility.TangentMode.Linear);
+            }
+        }
+        if (GUILayout.Button("Set Default FontSize"))
+        {
+            TextAsset ta = (TextAsset)serializedObject.targetObject;
+            Keyframe[] keyframes = new Keyframe[ta.dialogueMessage.Length];
+            for (int i = 0; i < ta.dialogueMessage.Length; i++)
+            {
+                keyframes[i].time = 0.1f * i;
+                keyframes[i].value = 30f;
+            }
+            ta.fontSizeCurve.keys = keyframes;
+            for (int i = 0; i < ta.dialogueMessage.Length; i++)
+            {
+                AnimationUtility.SetKeyLeftTangentMode(ta.fontSizeCurve, i, AnimationUtility.TangentMode.Linear);
+                AnimationUtility.SetKeyRightTangentMode(ta.fontSizeCurve, i, AnimationUtility.TangentMode.Linear);
+            }
+        }
+    }
 }
 #endif

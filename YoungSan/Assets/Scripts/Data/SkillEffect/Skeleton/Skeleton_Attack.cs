e841434bd7e0546d7497fcd48882b47b22fe8d4f
Deomyeon <deomyeon@gmail.com>
2022-06-30 19:54:20 +0900

    쨔쟌

--- YoungSan/Assets/Scripts/Data/SkillEffect/Skeleton/Skeleton_Attack.cs
+++ YoungSan/Assets/Scripts/Data/SkillEffect/Skeleton/Skeleton_Attack.cs
@@ -10,6 +10,7 @@ public class Skeleton_Attack : SkillEffect
         Stiff(hitEntity, 0.2f);
         ChangeColor(hitEntity, Color.red, 0f, 0.1f);
         KnockBack(hitEntity, direction, 0f, 0.1f, 8);
+        TickDamage("Poison", attackEntity, hitEntity, 0.2f, 1f);
     }
 
     protected override void ShowEnemyEffect(Entity attackEntity, Entity hitEntity, Vector2 direction, int index)
@@ -17,10 +18,12 @@ public class Skeleton_Attack : SkillEffect
         Stiff(hitEntity, 0.4f);
         ChangeColor(hitEntity, Color.red, 0f, 0.1f);
         KnockBack(hitEntity, direction, 0f, 0.2f, 8);
+        TickDamage("Poison", attackEntity, hitEntity, 0.2f, 1f);
     }
 
     protected override void ShowBossEffect(Entity attackEntity, Entity hitEntity, Vector2 direction, int index)
     {
         ChangeColor(hitEntity, Color.red, 0f, 0.1f);
+        TickDamage("Poison", attackEntity, hitEntity, 0.2f, 1f);
     }
 }
